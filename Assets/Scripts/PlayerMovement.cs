95ded9d228c14f645b518d3a221838091557e1f9
Andrew Huntington <andrew.huntington@gmail.com>
2023-01-18 14:08:55 +0900

    Add climbing animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,27 @@ public class PlayerMovement : MonoBehaviour
 {
   [SerializeField] float runSpeed = 10f;
   [SerializeField] float jumpSpeed = 5f;
+  [SerializeField] float climbSpeed = 5f;
 
   Vector2 moveInput;
   Rigidbody2D myRigidbody;
   Animator myAnimator;
   CapsuleCollider2D myCapsuleCollider;
+  float gravityScaleAtStart;
 
   void Start()
   {
     myRigidbody = GetComponent<Rigidbody2D>();
     myAnimator = GetComponent<Animator>();
     myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+    gravityScaleAtStart = myRigidbody.gravityScale;
   }
 
   void Update()
   {
     Run();
     FlipSprite();
+    ClimbLadder();
   }
 
   void OnMove(InputValue value)
@@ -61,5 +65,22 @@ public class PlayerMovement : MonoBehaviour
     {
       transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
     }
+  }
+
+  void ClimbLadder()
+  {
+    if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+    {
+      myRigidbody.gravityScale = gravityScaleAtStart;
+      return;
+    }
+
+    Vector2 climbVelocity = new Vector2(myRigidbody.velocity.x, moveInput.y * climbSpeed);
+    myRigidbody.velocity = climbVelocity;
+    myRigidbody.gravityScale = 0;
+
+    bool playerHasVerticalSpeed = Mathf.Abs(myRigidbody.velocity.y) > Mathf.Epsilon;
+
+    myAnimator.SetBool("isClimbing", playerHasVerticalSpeed);
   }
 }
