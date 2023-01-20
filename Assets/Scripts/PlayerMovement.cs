bc953139b4c397ee84994198c4b9e59527ce12de
Andrew Huntington <andrew.huntington@gmail.com>
2023-01-20 12:56:11 +0900

    Add shooting

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,8 @@ public class PlayerMovement : MonoBehaviour
   [SerializeField] float jumpSpeed = 5f;
   [SerializeField] float climbSpeed = 5f;
   [SerializeField] Vector2 deathKick = new Vector2(10f, 10f);
+  [SerializeField] GameObject bullet;
+  [SerializeField] Transform gun;
 
   Vector2 moveInput;
   Rigidbody2D myRigidbody;
@@ -55,6 +57,12 @@ public class PlayerMovement : MonoBehaviour
     {
       myRigidbody.velocity += new Vector2(0f, jumpSpeed);
     }
+  }
+
+  void OnFire(InputValue value)
+  {
+    if (!isAlive) { return; }
+    Instantiate(bullet, gun.position, transform.rotation);
   }
 
   void Run()
