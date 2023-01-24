57e7ee721599380460de52e1b6a99ac2d5f0cfb3
Andrew Huntington <andrew.huntington@gmail.com>
2023-01-24 10:37:18 +0900

    Remove extra stuff from level 1

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+  [SerializeField] float levelLoadDelay = 1f;
+
+  private void OnTriggerEnter2D(Collider2D other)
+  {
+    if (other.tag == "Player")
+    {
+      StartCoroutine(LoadNextLevel());
+    }
+  }
+
+  IEnumerator LoadNextLevel()
+  {
+    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    int nextSceneIndex = currentSceneIndex + 1;
+
+    if (nextSceneIndex != SceneManager.sceneCountInBuildSettings)
+    {
+      yield return new WaitForSecondsRealtime(levelLoadDelay);
+      SceneManager.LoadScene(nextSceneIndex);
+    }
+    else
+    {
+      yield return new WaitForSecondsRealtime(0.075f);
+      SceneManager.LoadScene(0);
+
+    }
+  }
+}
